d43da9c9c16fb9751eafdddaa77b3febb33268e9
MuffinPlease <benjamintec3455@gmail.com>
2022-05-06 22:51:18 -0500

    Header error

--- src/templates/Header.template.js
+++ src/templates/Header.template.js
@@ -1,5 +1,5 @@
 const Header = () => (
-    `<h1 class="header-title">MathZone</h1>
+    `<h1 class="header-title"><a href='#/'>MathZone</a></h1>
     <nav class = "header-nav">
         <a href='#/'>Inicio</a>
     </nav>`
